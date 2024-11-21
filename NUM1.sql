476762605f1d17200afec79e133d0cefd3631b47
Gulin-dev <GulinEgorEvgenevich@gmail.com>
2024-11-21 18:46:18 +0300

    Update NUM1.sql

--- NUM1.sql
+++ NUM1.sql
@@ -6,20 +6,22 @@ CREATE TABLE ST.GUEG_SALARY_HIST (
     gueg_effective_to DATE
 );
 
+WITH RankedSalaries AS (
+    SELECT 
+        "person" AS gueg_person,
+        "class" AS gueg_class,
+        "salary" AS gueg_salary,
+        "dt" AS gueg_effective_from,
+        ROW_NUMBER() OVER (PARTITION BY "person" ORDER BY "dt") AS rn,
+        LAG("salary") OVER (PARTITION BY "person" ORDER BY "dt") AS prev_salary
+    FROM DE.HISTGROUP
+)
 INSERT INTO ST.GUEG_SALARY_HIST (gueg_person, gueg_class, gueg_salary, gueg_effective_from, gueg_effective_to)
 SELECT 
-    "person" AS gueg_person,
-    "class" AS gueg_class,
-    "salary" AS gueg_salary,
-    "effective_from" AS gueg_effective_from,
-    LEAD("effective_from", 1, '9999-12-31') OVER (PARTITION BY "person" ORDER BY "effective_from") - INTERVAL '1 DAY' AS gueg_effective_to
-FROM (
-    SELECT 
-        "person",
-        "class",
-        "salary",
-        "effective_from",
-        ROW_NUMBER() OVER (PARTITION BY "person" ORDER BY "effective_from") AS rn
-    FROM DE.HISTGROUP
-) AS subquery
-WHERE rn = 1 OR "salary" != LAG("salary") OVER (PARTITION BY "person" ORDER BY "effective_from");
+    gueg_person,
+    gueg_class,
+    gueg_salary,
+    gueg_effective_from,
+    LEAD(gueg_effective_from, 1, '9999-12-31') OVER (PARTITION BY gueg_person ORDER BY gueg_effective_from) - INTERVAL '1 DAY' AS gueg_effective_to
+FROM RankedSalaries
+WHERE rn = 1 OR gueg_salary != prev_salary;
